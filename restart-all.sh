7e44bd931c221ad0113e6dcc1d06a353fcd4ffc6
Lucas Baesso <lucas.baesso@weinnovate.com.br>
2024-02-18 18:10:40 +0100

    Change restart script

--- restart-all.sh
+++ restart-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stop and remove containers
-docker-compose down
+docker compose down
 
 # Build and start containers in detached mode
-docker-compose up -d --build
+docker compose up -d --build
